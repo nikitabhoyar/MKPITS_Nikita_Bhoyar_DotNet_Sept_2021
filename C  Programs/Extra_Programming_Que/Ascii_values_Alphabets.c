d1f81ecd86cb8029b7193168e0dfe9281d51ed5c
Nikita <nikitabhoyar99@gmail.com>
2021-11-27 13:34:35 +0530

    New Program are modified and uploaded

---
+++ C  Programs/Extra_Programming_Que/Ascii_values_Alphabets.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        printf(" The Ascii value of alphbet is %d ", c);
+    return 0;
+}
---
+++ C  Programs/Extra_Programming_Que/Increment_Decrement_Operator_Example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   int var1 = 5, var2 = 5;
+
+   // 5 is displayed
+   // Then, var1 is increased to 6.
+   printf("%d\n", var1++);
+
+   // var2 is increased to 6 
+   // Then, it is displayed.
+   printf("%d\n", ++var2);
+
+   return 0;
+}
