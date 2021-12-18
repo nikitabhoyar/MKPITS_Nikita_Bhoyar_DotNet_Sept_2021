7575a2b3df277711f91ee1bf081008713e782268
Nikita <nikitabhoyar99@gmail.com>
2021-12-18 15:55:30 +0530

    C sharo folder is created

--- Sql Server Queries/C_Nov_Constraint_query.sql
+++ Sql Server Queries/C_Nov_Constraint_query.sql
@@ -1,1 +1,15 @@
 
+my Db;
+
+create table Constraints(Id int primary key identity(11,1) , Firstname varchar(20)not null , Lastname varchar(20)  default('Kumar'));
+insert into Constraints  (Firstname , Lastname ) values ('Nikita','Kale');
+
+delete from Constraints where Lastname = 'Kale';
+
+insert into Constraints (Firstname) values('Pushpa');
+
+alter table Constraints add DateHired datetime;
+
+update  Constraints set DateHired = getdate() where Lastname = 'Kumar';
+
+select * from Constraints;
