014fa11f3b2cb02dacafc05a164188827967ae98
Nikita <nikitabhoyar99@gmail.com>
2021-12-19 00:51:27 +0530

    Assignment operator program is modified

---
+++ JavaScript/9_Nov_Assignment/H_Nov3_Assignmentoperator.js
@@ -0,0 +1,34 @@
+
+
+let a = 5;
+let b = 6;
+
+document.write("The value of a is " , +a);
+console.log("<br>The value of a is ", +a);
+
+a+=6;
+document.write("<br>The value of addition assignment  a is " , +a);
+console.log("<br>The value of addition assignment a is ", +a);
+
+a-=6;
+document.write("<br>The value of Subtraction assignmnet of  a is " , +a);
+console.log("<br>The value of Subtraction assignmnet of a is ", +a);
+ 
+a*=8;
+document.write("<br>The value of Multiplication assignment of  a is " , +a);
+console.log("<br>The value of Multiplication assignmnet of a is ", +a);
+
+a/=8;
+document.write("<br>The value of Division assignment of  a is " , +a);
+console.log("<br>The value of Division assignmnet of a is ", +a);
+
+
+a%=8;
+document.write("<br>The value of Modulation assignment of  a is " , +a);
+console.log("<br>The value of Modulation assignmnet of a is ", +a);
+
+a**=8;
+document.write("<br>The value of Exponention assignment of  a is " , +a);
+console.log("<br>The value of Exponention assignmnet of a is ", +a);
+
+
