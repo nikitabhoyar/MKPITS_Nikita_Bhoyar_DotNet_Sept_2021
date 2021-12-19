94a1f5cbbbc0e51309bfd207bb9c5d0c37e585d9
Nikita <nikitabhoyar99@gmail.com>
2021-12-19 12:47:27 +0530

    String and for loop program is uploaded

---
+++ JavaScript/9_Nov_Assignment/H_Nov9_For_Loop.js
@@ -0,0 +1,6 @@
+
+for (var input = 1; input <= 10; input++)
+ {
+    document.write("<br>" , input);
+    console.log(input);
+   }
---
+++ JavaScript/9_Nov_Assignment/H_Nov9_Stringoperator.js
@@ -0,0 +1,5 @@
+var FirstName = "Bill";
+var LastName = "Gates"
+var FullName = FirstName + "   " + LastName;
+document.write("Fullname= " +FullName);
+console.log("Fullname");
