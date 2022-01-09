4b2be730c143626dfd6b51ff080f0dc1b17503b8
Nikita <nikitabhoyar99@gmail.com>
2022-01-09 13:24:50 +0530

    WebForm.aspx.cs files are uploaded

---
+++ ASP.Net/Client_Server_Site_Example/WebForm2.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Client_server_Site_Example
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm3.aspx?name="+TextBox1.Text+"&adr="+TextBox2.Text);
+        }
+    }
+}
