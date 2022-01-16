c4511954c8ded9f752137bcd6fb73c815aa47eda
Nikita <nikitabhoyar99@gmail.com>
2022-01-16 16:12:15 +0530

    New Folder is created

---
+++ MVC/MVC_Forms_Collection_Study/Controller/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Form_Collection_Study.Models;
+
+namespace MVC_Form_Collection_Study.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDBEntities db;
+        // GET: Employee
+        public ActionResult Index()
+        {
+            db = new EmployeeDBEntities();
+            return View(db.Employees.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(FormCollection formCollection)
+        {
+            if (ModelState.IsValid)
+              
+                {
+                Employee employee = new Employee();
+                employee.fname = formCollection["fname"];
+                employee.fname = formCollection["lname"];
+                employee.salary = Convert.ToInt32(formCollection["salary"]);
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                }
+            return View();
+        }
+    }
+}
