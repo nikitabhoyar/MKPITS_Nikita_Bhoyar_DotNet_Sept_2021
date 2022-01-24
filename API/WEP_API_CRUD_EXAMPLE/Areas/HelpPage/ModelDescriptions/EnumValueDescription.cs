d170d914414b5f8110624ee79436cdbc637a0235
Nikita <nikitabhoyar99@gmail.com>
2022-01-24 12:33:44 +0530

    WEB_API_CRUD_EXAMPLE

---
+++ API/WEP_API_CRUD_EXAMPLE/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace WEP_API_CRUD_EXAMPLE.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
