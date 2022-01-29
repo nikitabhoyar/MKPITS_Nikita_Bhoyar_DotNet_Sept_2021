0554816056ee50cdbb599175443f40d2da5f098f
Nikita <nikitabhoyar99@gmail.com>
2022-01-29 18:35:33 +0530

    Assignments of creating tables is uploaded

---
+++ Sql Server Queries/10_Nov_Assignments/H_Nov_Employee _Table_Details.sql
@@ -0,0 +1,10 @@
+use myDB;
+
+create Table Employee (id int primary key , firstname varchar(20) , lastname varchar(20) , branch varchar(10) , city varchar(10));
+insert into Employee values (101 , 'nikita' , 'bhoyar' , 'CSE' , 'Mumbai' );
+insert into Employee values (102 , 'pratiksha' , 'sharma' , 'ETC' , 'Mumbai' );
+insert into Employee values (103 , 'nikhil' , 'Nagmote' , 'ME' , 'Delhi' );
+insert into Employee values (104 , 'pooja' , 'BHande' , 'CSE' , 'Banglore' );
+insert into Employee values (105 , 'rutu' , 'Dhanore' , 'etc' , 'Nagpur' );
+
+
---
+++ Sql Server Queries/10_Nov_Assignments/H_Nov_Student_Creted_Table.sql
@@ -0,0 +1,2 @@
+create table Student(id int primary key  , firstname varchar(20) , lastname varchar(20) , branch varchar(10) , city varchar(10) );
+select * from Student;
