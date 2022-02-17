4790dcea1ee7dded805d5abf5feee6f9c3a19628
Nikita <nikitabhoyar99@gmail.com>
2022-02-17 15:05:14 +0530

    Mother Inheritance program is uploaded

---
+++ C Sharp/C_Dec1_Inheritance_Mother.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C_Dec1_Inheritance_Mother
+{
+    class Mother
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Mother()
+        {
+            Age = 45;
+            Name = "Varsha";
+            Console.WriteLine("Mother Object Created");
+
+        }
+        public Mother(int age, string name)
+        {
+            Age = age;
+            Name = name;
+            Console.WriteLine("Mother Object Created");
+        }
+        public override string ToString()
+        {
+            return " Mother's Name " + Name + " Age " + Age;
+        }
+    }
+}
