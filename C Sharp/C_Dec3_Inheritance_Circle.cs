f8a090799067d929dff01b4f853ace8f30a2aa06
Nikita <nikitabhoyar99@gmail.com>
2022-02-18 04:02:57 +0530

    Circle Inheritance Program is uploaded

---
+++ C Sharp/C_Dec3_Inheritance_Circle.cs
@@ -0,0 +1,28 @@
+﻿using C_Dec3_Inheritance_Circle_Program;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec3_Inheritance_Circle_Program
+{
+
+    /// THIS IS THE EXAMPLE OF MULTILEVEL INHERICTANCE 
+
+    class Circle : Point
+    {
+        protected int Radius { get; set; }
+
+        public Circle()
+        {
+            Radius = 10;
+        }
+        public virtual double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public Circle(int radius, int x, int y) : base(x, y)
+        {
+            Radius = radius;
+        }
+    }
+}
