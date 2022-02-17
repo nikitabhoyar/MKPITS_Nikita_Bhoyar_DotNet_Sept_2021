fd1c9ba6908dcbf2d8a729d1897cf0be6c8a2585
Nikita <nikitabhoyar99@gmail.com>
2022-02-17 16:07:51 +0530

    Inheritance of Father_Son_Father program is uploaded

---
+++ C Sharp/C_Dec1_Inheritance_Father_Son_Father.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec1_Inheritance_Father_Son_Father
+{
+    class Father
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Father()
+        {
+            Age = 45;
+            Name = "Devanand";
+            // Console.WriteLine("Father Object Created");
+
+        }
+        public Father(int age, string name)
+        {
+            Age = age;
+            Name = name;
+            Console.WriteLine("Father Object Created");
+        }
+        public override string ToString()
+        {
+            return " Father's Name " + Name + " Age " + Age;
+        }
+    }
+}
