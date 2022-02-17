25cd844caabf6b58bb9f5744b17451780f2185e8
Nikita <nikitabhoyar99@gmail.com>
2022-02-18 03:10:19 +0530

    Exception Handling program is uploaded

---
+++ C Sharp/C_Dec3_Exception_Handling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_Dec3_Exception_handling
+{
+    class Program
+    {
+        // 1 SYNTAX ERROR -Programming
+        // 2 Logical error -testing 
+        // run time error - can not be removed (avoided)
+        //Exception
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 10, y = 0;
+                Console.WriteLine(x / y);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
