b968aa4349be398c0860bad70566586d4e6c941b
Nikita <nikitabhoyar99@gmail.com>
2022-02-17 16:24:24 +0530

    Inheritance of Father_Son_Program

---
+++ C Sharp/C_Dec1_Inheritance_Father_Son_Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace C_Dec1_Inheritance_Father_Son_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Father f1 = new Father();
+            Console.WriteLine(f1);
+
+            Son s1 = new Son();
+            Console.WriteLine(s1);
+
+            //Son s2 = new Son(15, "Ayush", 18, "Manish");
+            //Console.WriteLine(s2);
+        }
+    }
+}
