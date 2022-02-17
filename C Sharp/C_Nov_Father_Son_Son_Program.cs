2acbc283e184ce3005e52d9e1bfad8a978b13c4c
Nikita <nikitabhoyar99@gmail.com>
2022-02-17 20:33:59 +0530

    Inheritance Father Son Son Program

---
+++ C Sharp/C_Nov_Father_Son_Son_Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec1_Inheritance_Father_Son_Son_Program
+{
+    internal class Son : Father
+    {
+        public int S_age { get; set; }
+        public string S_Name { get; set; }
+
+        public Son() : base() //default constructor
+        {
+            S_age = 21;
+            S_Name = "Nikita";
+            // Console.WriteLine("Son object Created");
+        }
+        public Son(int Sage, string Sname, int Fage, string Fname) : base(Fage, Fname)
+        {
+            S_age = Sage;
+            S_Name = Sname;
+            Console.WriteLine("Son object Created");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " Son's Name " + S_Name + " Age  " + S_age;
+        }
+    }
+}
