83a4e25066c67e02037a871426b991d25674ca13
Nikita <nikitabhoyar99@gmail.com>
2022-02-18 00:39:02 +0530

    Abstractclass Interface Circle Program is uploaded

---
+++ C Sharp/C_Dec3_Abstractclass_Interface_Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec3_Abstractclass_Interface
+{
+    class Circle: Shape
+    {
+        //Concrete class Circle
+        public int Radius { get; set; }
+        public Circle()
+        {
+            Radius = 10;
+        }              
+        
+        //abstract method is overridden these class.Area()
+        public Circle(int r)
+        {
+            Radius = r;
+        }
+        public override void Area()
+        {
+            Console.WriteLine("Area of Circle " + (Math.PI * Radius * Radius));
+        }
+    }
+}
