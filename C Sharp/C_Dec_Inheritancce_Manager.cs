46588c8aff141f6f6f24bac245c082ee3caeebc2
Nikita <nikitabhoyar99@gmail.com>
2022-02-17 14:31:39 +0530

    Manager Inheritance Program is uploaded

---
+++ C Sharp/C_Dec_Inheritancce_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec1_inheraictance
+{
+    class Manager : Employee
+    {
+        public decimal Commission { get; set; }
+        public int Saleqty { get; set; }
+
+        public Manager() : base()
+        {
+            Commission = 350;
+            Saleqty = 10;
+        }
+        public Manager(decimal commission, int salary, int id, string name, decimal sal) : base(id, name, sal)
+        {
+            Commission = 250;
+            Saleqty = 10;
+        }
+
+        //public override void GrossSalary()
+        //{
+        //    Console.WriteLine("Manager's Gross Salary"+(BasicSal+Commission *Saleqty));
+        //}
+        public override decimal GrossSalary()
+        {
+            return (BasicSal + Commission * Saleqty);
+        }
+
+    }
+}
