76075cb7373133eecfb4ef660f29a48f5ddf1c8d
Nikita <nikitabhoyar99@gmail.com>
2022-02-17 15:20:37 +0530

    Inheritance Program is uploaded

---
+++ C Sharp/C_Dec1_Inheritance_Program.cs
@@ -0,0 +1,21 @@
+﻿
+using C_Dec1_inheraictance;
+using System;
+
+namespace C_Dec1_Inheritance_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mother m1 = new Mother();
+            Console.WriteLine(m1);
+
+            Daughter d1 = new Daughter();
+            Console.WriteLine(d1);
+
+            Daughter d2 = new Daughter(15, "bina", 45, "tina");
+            Console.WriteLine(d2);
+        }
+    }
+}
