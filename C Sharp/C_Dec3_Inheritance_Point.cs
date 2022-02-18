b9914f12ac4f88475830b1982f15e332d5d5d8a8
Nikita <nikitabhoyar99@gmail.com>
2022-02-18 12:49:31 +0530

    Inheritance of Point Class program is uploaded

---
+++ C Sharp/C_Dec3_Inheritance_Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec3_Inheritance_Point
+{
+    class Point
+    {
+        protected int x { get; set; }
+        public int y { get; set; }
+
+        public Point()
+        {
+            x = 10;
+            y = 10;
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
