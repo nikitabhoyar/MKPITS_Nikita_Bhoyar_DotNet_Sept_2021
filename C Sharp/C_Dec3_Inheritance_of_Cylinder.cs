d7d3174a6de14efb1cc633c769111647f05645b9
Nikita-Bhoyar <nikitabhoyar99@gmail.com>
2022-02-18 12:42:51 +0530

    Update C_Dec3_Inheritance_of_Cylinder.cs

--- C Sharp/C_Dec3_Inheritance_of_Cylinder.cs
+++ C Sharp/C_Dec3_Inheritance_of_Cylinder.cs
@@ -8,17 +8,15 @@ namespace C_Dec3_Inheritance_Cylinder
     {
         public int Height { get; set; }
 
-        public Cylinder()  //default
+        public Cylinder()                                                  //default
         {
             Height = 100;
-         // Radius = 12; //radius is derived in classes
+            //Radius = 12;                                                 //radius is derived in classes
         }
-        public Cylinder(int h, int r, int x, int y) : base(r, x, y) //parameterised constructor
+        public Cylinder(int h, int r, int x, int y) : base(r, x, y)     //parameterised constructor
         {
             Height = h;
-
-        }
-        //2*pi*r*r+2(pi*r*r)+2(pi*r*h)
+        }                                                                //2*pi*r*r+2(pi*r*r)+2(pi*r*h)
         public new double Area()
         {
             return 2 * base.Area() + 2 * Math.PI * Radius * Height;
