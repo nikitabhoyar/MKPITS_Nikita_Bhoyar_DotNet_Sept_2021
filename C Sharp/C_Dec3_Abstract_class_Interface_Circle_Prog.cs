0b9dc90525ea9b44f66129467ab71a1b8cad066e
Nikita-Bhoyar <nikitabhoyar99@gmail.com>
2022-02-18 20:29:26 +0530

    Update C_Dec3_Abstract_class_Interface_Circle_Prog.cs

--- C Sharp/C_Dec3_Abstract_class_Interface_Circle_Prog.cs
+++ C Sharp/C_Dec3_Abstract_class_Interface_Circle_Prog.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace C_Dec3_Abstract_class_Interface_Circle_Prog
+namespace C_Dec3_Abstract_Class_Interface_Circle_Prog
 {
     class Circle: Shape
     {
