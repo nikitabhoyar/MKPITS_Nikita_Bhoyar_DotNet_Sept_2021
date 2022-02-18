7a3d479ee6dc40243080640e92b20bec39139ff8
Nikita <nikitabhoyar99@gmail.com>
2022-02-19 01:37:22 +0530

    Interface Example of Car Program

---
+++ C Sharp/C_Dec3_Interface_Example_Car_Prog.cs
@@ -0,0 +1,40 @@
+﻿using C_Dec3_Interface_Example;
+using System;
+
+namespace C_Dec3_Interface_Example_Car
+{
+    internal class Car : Vehicle, IPrintable, IDisplayble
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Seats { get; set; }
+        public int Speed { get; private set; }
+
+        public Car()
+        {
+            Brand = "Maruti";
+            Model = "Dezier";
+            Color = "silver Metal";
+            Seats = 6;
+        }
+        public void GetSpeed(int s)
+        {
+            Speed = s;
+        }
+        public override void ShowSpeed()
+        {
+            Console.WriteLine("Speed of this car " + Speed);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(" Model " + Model + " Brand " + Brand);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(" Color " + Color + " Seats " + Seats);
+        }
+    }
+}
