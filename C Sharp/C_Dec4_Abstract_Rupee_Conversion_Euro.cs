163427a83525f36745856e4bd2bea13ad7bcf13f
Nikita <nikitabhoyar99@gmail.com>
2022-02-19 13:05:10 +0530

    Abstract Rupee Conversion Prog is uploaded

---
+++ C Sharp/C_Dec4_Abstract_Rupee_Conversion_Euro.cs
@@ -0,0 +1,24 @@
+﻿using Bogus.DataSets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec4_Abstract_rupee_Conversion
+{
+    class Euro : Currency
+    {
+        public int E { get; set; }
+        public Euro()
+        {
+            E = 60;
+        }//abstract method is overridden these class .Area()
+        public Euro(int euro)
+        {
+            E = euro;
+        }
+        public override void ToRupees()
+        {
+            Console.WriteLine(" Euro = " + E + "\n Euro To Rupees Conversion = " + E * 84);
+        }
+    }
+}
