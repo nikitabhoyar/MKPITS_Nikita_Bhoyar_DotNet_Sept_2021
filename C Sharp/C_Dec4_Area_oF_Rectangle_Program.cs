defef03d9c1ca53944db281d4b5e62516abc3611
Nikita <nikitabhoyar99@gmail.com>
2022-02-19 23:56:23 +0530

    Area of Rectangle Program is uploaded

---
+++ C Sharp/C_Dec4_Area_oF_Rectangle_Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_Dec17_Area_of_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = 4;
+            double width = 2;
+            double area;
+            Console.WriteLine("lenght of Rdctangle = " + length);
+            Console.WriteLine("Width of Rdctangle = " + width);
+
+            area = length * width;
+            Console.WriteLine("Area of Rectangle = " + area);
+        }
+    }
+}
