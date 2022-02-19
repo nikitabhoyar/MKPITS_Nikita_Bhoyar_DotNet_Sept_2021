d117da5887976e086ee42476a4f4eff4341e2203
Nikita <nikitabhoyar99@gmail.com>
2022-02-19 12:09:32 +0530

    Vehicle Interface Program is uploaded

---
+++ C Sharp/C_Dec3_Interface_Example_Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec3_Interface_Example
+{
+    abstract class Vehicle
+    {
+        public int Speed { get; set; }
+        public abstract void ShowSpeed();
+    }
+}
