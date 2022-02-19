48e556e773fe6e029ca59217611f29dd419ddf18
Nikita-Bhoyar <nikitabhoyar99@gmail.com>
2022-02-19 13:17:34 +0530

    Update C_Dec4_Abstract_Rupee_Conversion_Euro_Prog.cs

--- C Sharp/C_Dec4_Abstract_Rupee_Conversion_Euro_Prog.cs
+++ C Sharp/C_Dec4_Abstract_Rupee_Conversion_Euro_Prog.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace C_Dec4_Abstract_Rupee_Conversion_Prog
+namespace C_Dec4_Abstract_Rupee_Conversion_Euro_Prog
 {
     class Euro : Currency
     {
