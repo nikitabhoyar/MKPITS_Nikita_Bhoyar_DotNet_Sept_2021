99622480eae9cb2db7e8ebf0eb9f6f225b26edbd
Nikita <nikitabhoyar99@gmail.com>
2022-02-19 11:40:51 +0530

    IPrintable Interface Program is uploaded

---
+++ C Sharp/C_Dec3_Interface_Example_Iprintable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec3_Interface_Example
+{
+    interface IDisplayble
+    {
+        void Display();
+    }
+
+}
