b625186461e35bc38aa197b3b561dd1ec13ee001
Nikita <nikitabhoyar99@gmail.com>
2022-02-19 20:26:28 +0530

    Pound to Rupee Conversion program is uploaded

---
+++ C Sharp/C_Dec4_Abstract_Rupee_Conversion_Pound_Prog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec4_Abstract_rupee_Conversion
+{
+    class Pound : Currency
+    {
+        public int p { get; set; }
+
+
+        public Pound()
+        {
+            p = 40;
+        }//abstract method is overridden these class .ToRupees()
+        public Pound(int pound)
+        {
+            p = pound;
+        }
+
+
+        public override void ToRupees()
+        {
+            Console.WriteLine(" pound =" + p + "\n Pound To Rupees Conversion = " + p * 5);
+        }
+    }
+}
