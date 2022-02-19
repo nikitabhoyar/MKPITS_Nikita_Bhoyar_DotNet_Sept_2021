bb18c6b880255f7eed5951309ceec0ec733c67fc
Nikita <nikitabhoyar99@gmail.com>
2022-02-19 23:34:02 +0530

    Collection of Student Program is uploaded

---
+++ C Sharp/C_Dec4_Collections_Student_Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec4_Collections_Student_Program
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return " Id " + Id + " Name " + Name;
+        }
+    }
+}
