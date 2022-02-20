76d69b5ec78a51dc56e6ea8cd172a83601d6b29a
Nikita <nikitabhoyar99@gmail.com>
2022-02-21 01:39:02 +0530

    We find Area's of Shapes by using Function Operator overloading.

---
+++ C Sharp/C_Dec4_Area_oF_Cir_Rect_Tri_Prog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace C_Dec19_Area_oF_Cir_Rect_Tri_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Function overloading : given extra task to the same function
+            //operator overloading : given extra task to an operator 
+            Console.WriteLine("Enter The Value Number");
+            Console.WriteLine("Enter  value of length");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter  value of Breadth");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter  value of Height");
+            double h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter  value of Radius");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Area(r);
+            Area(l, b);
+            Area(b, h);
+        }
+        public static void Area(int r)    //function defination
+        {
+            //double a;    --it can be also declare like these 
+            double a = Math.PI * r * r;
+            Console.WriteLine("The Area of Circle is ={0:f3}", a);          //f is upto 3 digit 
+        }
+        public static void Area(int l, int b)
+        {
+            int a = l * b;
+            Console.WriteLine("Area of Rectangle = " + a);
+        }
+        public static void Area(int l, double h)
+        {
+            double a1 = 0.5 * l * h;
+
+            Console.WriteLine("Area of Triangle ={0:f3} ", a1);
+        }
+    }
+}
