166880e5f9ccb673d669f69d5efc33bfeaec783a
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 17:58:27 +0530

    Area of Circle is find out by using For Loop

---
+++ C Sharp/C_Dec17_Area_oF_Circle_Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_Dec17_Area_oF_Circle_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int Radius;
+            for (i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Enter the radius of Circle : ");
+                Radius = Convert.ToInt32(Console.ReadLine());
+                // pie * Radius * Radius;
+                // Console.WriteLine("Area of Circle" + (Math.PI * Radius * Radius));
+            }
+        }
+
+    }
+}
