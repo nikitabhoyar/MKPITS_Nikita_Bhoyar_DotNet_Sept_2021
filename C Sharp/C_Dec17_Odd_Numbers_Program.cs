689510666ea640b409edf39b2bd514a5a4dea457
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 23:01:45 +0530

    Odd Numbers is printed in between of 1 to 100 by using some logic

---
+++ C Sharp/C_Dec17_Odd_Numbers_Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace C_Dec17_Odd_Numbers_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            Console.WriteLine("Enter the Numbers");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)           ///odd condition
+                {
+                    j++;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Odd number are" + j);    //count of odd number 
+        }
+    }
+}
