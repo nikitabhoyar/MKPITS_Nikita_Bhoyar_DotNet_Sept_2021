c75949801de095690654b150b42d4c7912326848
Nikita <nikitabhoyar99@gmail.com>
2022-02-21 02:22:32 +0530

    Lcm of Two Numbers Prog is Uploaded by using If Condition.

---
+++ C Sharp/C_Dec19_Lcm_oF_Two_Num_Prog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_Dec19_Lcm_of_Two_Num_Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i;
+            Console.WriteLine("Enter two number ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= a * b; i++)
+                if (i % a == 0 && i % b == 0)
+                    break;
+            {
+                Console.WriteLine("The Lcm is {0}", i);
+            }
+        }
+    }
+}
