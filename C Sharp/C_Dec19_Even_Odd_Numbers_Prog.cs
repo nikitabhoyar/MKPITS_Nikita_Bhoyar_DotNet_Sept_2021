321cbcdb2f02b1c087ddebf7de18416984e361c3
Nikita <nikitabhoyar99@gmail.com>
2022-02-21 01:08:22 +0530

    This prog find even and odd numbers by using If Else If logic

---
+++ C Sharp/C_Dec19_Even_Odd_Numbers_Prog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_Dec19_Even_Odd_Using_IfElse_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter any number");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("The number is zero\n");
+            }
+            else if (n >= 0)
+            {
+                Console.WriteLine("the number is positve\n");
+            }
+            else
+            {
+                Console.WriteLine("the number is negative\n");
+            }
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("the number is even\n");
+            }
+            else
+            {
+                Console.WriteLine("the number is odd\n");
+            }
+        }
+    }
+}
