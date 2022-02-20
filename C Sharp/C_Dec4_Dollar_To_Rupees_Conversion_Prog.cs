739c32a918ceaa4f2de7b4a490d36de8736973a3
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 17:18:20 +0530

    Dollar To Rupees Conversion Program is Uploaded

---
+++ C Sharp/C_Dec4_Dollar_To_Rupees_Conversion_Prog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Dec4_Dollar_To_Rupees_Conversion_Prog
+{
+    class Dollar : Currency
+    {
+        public int dollar { get; set; }
+        public Dollar()
+        {
+            dollar = 50;
+        }//abstract method is overridden these class .Area()
+        public Dollar(int d)
+        {
+            dollar = d;
+        }
+        public override void ToRupees()
+        {
+            Console.WriteLine(" Dollar = " + dollar + "\n Dollar To Rupees Conversion = " + dollar * 74.64m);
+        }
+    }
+}
