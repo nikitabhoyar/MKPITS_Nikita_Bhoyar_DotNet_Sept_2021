71380c3c67a12d8e58a3ce57568886de937fde89
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 22:18:11 +0530

    Number increment decrement program is uploaded

---
+++ C Sharp/C_Dec17_Value_Increment_Decrement_Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_Dec17_Operator_Value_Increment_Decrement_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10, j = 8;
+            Console.WriteLine("the value before + operator =" + i);
+            i++;
+            Console.WriteLine("the value After + operator =" + i);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("the value before - operator =" + j);
+            j--;
+            Console.WriteLine("the value After - operator =" + j);
+        }
+    }
+}
