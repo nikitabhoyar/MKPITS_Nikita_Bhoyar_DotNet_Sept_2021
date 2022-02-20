df5251a78c8e5083bcf63e1fdd9fb7d60762bb33
Nikita <nikitabhoyar99@gmail.com>
2022-02-21 02:27:48 +0530

    The program is implemented by using For loop

---
+++ C Sharp/C_Dec19_Multiplication_Table_Prog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_Dec19_Multiplication_Table_Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.WriteLine("enter the number ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < 10; i++)
+            {
+                Console.Write("{0} X {1} = {2} \n", n, i, n * i);   //concept of table 
+            }
+        }
+    }
+}
