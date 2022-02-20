ee4968df35ddd454d80c56ff780fbf3f0bbfc480
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 18:18:52 +0530

    We found here even numbers between 1 to 100

---
+++ C Sharp/CDec17_Printing_Even_Numbers_Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Dec17_Even_Numbers_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            Console.WriteLine("Enter the number ");
+            for (int i = 2; i <= 100; i++)
+            {
+                if (i % 2 == 0)                 //even logic
+                    j++;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("The even no are " + j);  //for count 
+        }
+    }
+}
