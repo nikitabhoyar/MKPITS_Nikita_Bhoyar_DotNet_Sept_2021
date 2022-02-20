1179383943bfe0c92b477e026139f77baa7c3eb8
Nikita <nikitabhoyar99@gmail.com>
2022-02-21 03:43:43 +0530

    Starpyramid Program is uploaded

---
+++ C Sharp/C_Dec19_StarPyramid_Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Dec19_StarPyramid_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(" * ");    //to recall write 
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
