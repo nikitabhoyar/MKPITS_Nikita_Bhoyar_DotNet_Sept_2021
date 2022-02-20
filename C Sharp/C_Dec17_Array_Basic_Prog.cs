71998354e23d2b790dde3e42b3220df83fd300bf
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 17:33:25 +0530

    Array Basic Program is Uploaded

---
+++ C Sharp/C_Dec17_Array_Basic_Prog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_Dec17_Array_Basic_Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[5];
+            Console.WriteLine("Enter the Marks of Stuent ");
+            for (int i = 0; i < 5; i++)
+            {
+                marks[i] = Convert.ToInt32(Console.ReadLine());     //we can convert here upar bhi int declare and convert me bhi
+
+            }
+            Console.WriteLine("The NUmbers Are as Follows");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(marks[i]);
+            }
+        }
+    }
+}
