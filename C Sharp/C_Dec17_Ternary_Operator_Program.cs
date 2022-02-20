20fdf3b6ed3e553cac3e92db74196a1b8999f853
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 23:08:36 +0530

    Ternary Operator Program is uploaded

---
+++ C Sharp/C_Dec17_Ternary_Operator_Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_Dec17_Ternary_Operator_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4, j = 2, k;
+            Console.WriteLine("Ternary Operation");
+            Console.WriteLine("Value of i = " + i);
+            Console.WriteLine("Value of i = " + j);
+            k = (i--) * (i--) + (2 * j);
+            Console.WriteLine("the value are k = " + k);
+        }
+        // Console.WriteLine("the value are k" + k);
+    }
+}
