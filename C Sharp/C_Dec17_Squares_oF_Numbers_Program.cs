47430e7b242f67801eab4092c99bbf69ed0b7d19
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 21:31:14 +0530

    Squares oF Numbers Program is Uploaded

---
+++ C Sharp/C_Dec17_Squares_oF_Numbers_Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_Dec17_Squares_oF_Numbers_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("Enter the number upto which you want Square of number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Square series of number");
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i * i);
+            }
+        }
+    }
+}
