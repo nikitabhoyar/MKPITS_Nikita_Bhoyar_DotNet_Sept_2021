859fdc0656bab5ecd5ac94bde8048fc58c90f736
Nikita <nikitabhoyar99@gmail.com>
2022-02-20 21:49:59 +0530

    Nested Decrement Pattern Program is uploaded

---
+++ C Sharp/C_Dec17_Nested_Decrement_Pattern_Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Dec17_Nested_Decrement_Pattern_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 5; i >= 1; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(i);      //to display as it is the value of i
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
