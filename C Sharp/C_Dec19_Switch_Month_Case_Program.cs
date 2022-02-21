08f014c91a65c3dfa7c8217ef948a88a64d1380f
Nikita <nikitabhoyar99@gmail.com>
2022-02-21 23:42:55 +0530

    Switch Month Case Program is uploaded

---
+++ C Sharp/C_Dec19_Switch_Month_Case_Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace C_Dec19_Switch_Month_Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month;
+            Console.WriteLine("Enter Any Number Between 1 to 12");
+            month = Convert.ToInt32(Console.ReadLine());
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("Feb");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+                    Console.WriteLine("July");
+                    break;
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+            }
+        }
+    }
+}
