da52cf04dad3839574c3319ef73575adcd137194
Nikita <nikitabhoyar99@gmail.com>
2022-02-21 16:42:05 +0530

    Last Digit Number Program is uploaded

---
+++ C Sharp/C_Dec19_Last_Digit_Delete_Number_Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_Dec19_Last_Digit_Delete_Number_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the Number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            num = num / 10;
+            Console.WriteLine("After deleting last Digit Number is = " + num);
+        }
+    }
+}
---
+++ C Sharp/C_Dec19_Swap_Numbers_Prog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_Dec19_Swap_Numbers_Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int a, b;
+                Console.WriteLine("Enter two number");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The number before swaping is {0} {1}", a, b);
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                Console.WriteLine("After swaping no is {0} {1}", a, b);
+            }
+        }
+    }
+}
